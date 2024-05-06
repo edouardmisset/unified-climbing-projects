cace5a1f8ca7f9fa2ad96a5be554ec3bab2c2049
Edouard Misset <edouardmisset@gmail.com>
2024-05-06 22:41:07 +0200

    style: remove unused code from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import Link from 'next/link'
-
-import './reset.css'
 import styles from './index.module.css'
 
 export default async function Home() {
@@ -8,20 +5,6 @@ export default async function Home() {
     <main className={styles.main}>
       <div className={styles.container}>
         <h1 className={styles.title}>Climbing App</h1>
-        <div className={styles.linkContainer}>
-          <Link className={styles.link} href="./qr-code/training">
-            <h3>Training QR Code</h3>
-          </Link>
-          <Link className={styles.link} href="./qr-code/ascents">
-            <h3>Ascent QR Code</h3>
-          </Link>
-          <Link
-            className={styles.link}
-            href={`./visualization/training/${new Date().getFullYear()}`}
-          >
-            <h3>Training Visualization</h3>
-          </Link>
-        </div>
       </div>
     </main>
   )
