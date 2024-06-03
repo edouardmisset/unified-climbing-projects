61fa88da2c77fc9cc22e376c6b1880d48a4bea16
Edouard Misset <edouardmisset@gmail.com>
2024-06-03 20:58:26 +0200

    feat: add all fields to the form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,80 +1,88 @@
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-import '~/styles/globals.css'
+import "~/styles/globals.css";
 
-import { TRPCReactProvider } from '~/trpc/react'
+import { TRPCReactProvider } from "~/trpc/react";
 
-import './reset.css'
-import './quick-upgrades.css'
-import './global-styles.css'
-import Link from 'next/link'
-import Sync from './_components/sync-button/sync-button'
+import "./reset.css";
+import "./quick-upgrades.css";
+import "./global-styles.css";
+import Link from "next/link";
+import Sync from "./_components/sync-button/sync-button";
+import { ThemeProvider } from "./_components/theme-provider";
 
 export const metadata = {
-  title: 'Unified Climbing Projects',
-  description: 'Collection of Climbing Visualization pages',
-  icons: [{ rel: 'icon', url: '/favicon.ico' }],
-}
+  title: "Unified Climbing Projects",
+  description: "Collection of Climbing Visualization pages",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  const thisYear = new Date().getFullYear()
+  const thisYear = new Date().getFullYear();
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <TRPCReactProvider>
-          <header>
-            <nav>
-              <ul
-                className="flex-row wrap gap flex-center flex-even margin-auto"
-                style={{
-                  minHeight: '3rem',
-                  listStyle: 'none',
-                }}
-              >
-                <li>
-                  <Link href={'/'}>Home</Link>
-                </li>
-                <li>
-                  <Link href={'/log'}>Log</Link>
-                </li>
-                <li>
-                  <Link href={'/qr-code/ascents'}>Ascents QR</Link>
-                </li>
-                <li>
-                  <Link href={'/barcode/ascents'}>Ascents Barcode</Link>
-                </li>
-                <li>
-                  <Link href={`/visualization/ascents/${thisYear}`}>
-                    Ascents Visualization
-                  </Link>
-                </li>
-                <li>
-                  <Link href={'/qr-code/training'}>Training QR</Link>
-                </li>
-                <li>
-                  <Link href={`/barcode/training/${thisYear}`}>
-                    Training Barcode
-                  </Link>
-                </li>
-                <li>
-                  <Link href={`/visualization/training/${thisYear}`}>
-                    Training Visualization
-                  </Link>
-                </li>
-                <li>
-                  <Sync />
-                </li>
-              </ul>
-            </nav>
-          </header>
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <header>
+              <nav>
+                <ul
+                  className="wrap gap flex-center flex-even margin-auto flex-row"
+                  style={{
+                    minHeight: "3rem",
+                    listStyle: "none",
+                  }}
+                >
+                  <li>
+                    <Link href={"/"}>Home</Link>
+                  </li>
+                  <li>
+                    <Link href={"/log"}>Log</Link>
+                  </li>
+                  <li>
+                    <Link href={"/qr-code/ascents"}>Ascents QR</Link>
+                  </li>
+                  <li>
+                    <Link href={"/barcode/ascents"}>Ascents Barcode</Link>
+                  </li>
+                  <li>
+                    <Link href={`/visualization/ascents/${thisYear}`}>
+                      Ascents Visualization
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={"/qr-code/training"}>Training QR</Link>
+                  </li>
+                  <li>
+                    <Link href={`/barcode/training/${thisYear}`}>
+                      Training Barcode
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={`/visualization/training/${thisYear}`}>
+                      Training Visualization
+                    </Link>
+                  </li>
+                  <li>
+                    <Sync />
+                  </li>
+                </ul>
+              </nav>
+            </header>
+            {children}
+          </ThemeProvider>
         </TRPCReactProvider>
         <SpeedInsights />
       </body>
     </html>
-  )
+  );
 }
--- src/app/log/page.tsx
+++ src/app/log/page.tsx
@@ -1,3 +1,179 @@
+"use client";
+
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { Slider } from "~/app/_components/ui/slider";
+import {
+  ROUTE_GRADE_TO_NUMBER,
+  convertGradeToNumber,
+  convertNumberToGrade,
+  type RouteGrade,
+} from "~/helpers/converter";
+import { cn } from "~/lib/utils";
+import { ThemeProvider } from "../_components/theme-provider";
+
+const numberGrades = [...ROUTE_GRADE_TO_NUMBER.values()];
+
+type AscentDescription = {
+  topoGrade: number;
+  routeName: string;
+  routeOrBoulder: "route" | "boulder";
+  crag: string; // look up in DB
+  date: string; // yyyy-mm-dd
+  holds?: string; // should be an enum : Jugs, crimps, pockets, slopers...
+  height?: number; // from 0 to 100 m
+  rating?: number; // from 0 to 5*
+  profile?: string; // should be an enum : Vertical, overhang, slab...
+  comments?: string;
+};
+
 export default function Log(): React.JSX.Element {
-  return <div>hi</div>
+  const climberAverageGrade: RouteGrade = "7b";
+
+  const defaultTopoGrade = convertGradeToNumber(climberAverageGrade);
+  const now = new Date();
+  const { handleSubmit, register, setValue, watch } =
+    useForm<AscentDescription>({
+      defaultValues: {
+        topoGrade: defaultTopoGrade,
+        date: `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, "0")}-${now.getDate().toString().padStart(2, "0")}`,
+        holds: "crimps",
+        routeOrBoulder: "route",
+        profile: "vertical",
+      },
+    });
+
+  const onSubmit: SubmitHandler<AscentDescription> = (data) =>
+    console.log(data);
+
+  return (
+    <ThemeProvider forcedTheme="light">
+      <div className="m-auto [width:_clamp(40ch,50vw,80ch)]">
+        <form
+          className={cn("flex flex-col gap-5 p-4")}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <h2 className="mb-6 w-full text-center text-4xl">Congrats 🎉</h2>
+          <fieldset className="flex flex-col gap-4">
+            The Basics
+            <label htmlFor="date" className="flex flex-col gap-2">
+              Date
+              <input required {...register("date")} type="date" title="Date" />
+            </label>
+            <label htmlFor="routeName" className="flex flex-col gap-2">
+              Route Name
+              <input
+                required
+                id="routeName"
+                autoComplete="off"
+                {...register("routeName")}
+                placeholder="The name of the route or boulder climbed"
+                title="Route Name"
+              />
+            </label>
+            <label htmlFor="routeName" className="flex flex-col gap-2">
+              Route or Boulder
+              <select
+                id="routeOrBoulder"
+                {...register("routeOrBoulder")}
+                title="Route or Boulder"
+              >
+                <option value="route" defaultChecked>
+                  Route
+                </option>
+                <option value="boulder">Boulder</option>
+              </select>
+            </label>
+            <label htmlFor="crag" className="flex flex-col gap-2">
+              Crag
+              <input
+                required
+                id="crag"
+                {...register("crag")}
+                placeholder="The name of the crag"
+                title="Crag Name"
+              />
+            </label>
+            <label htmlFor="topoGrade" className="flex flex-col gap-2">
+              Topo Grade {convertNumberToGrade(watch("topoGrade"))}
+              <Slider
+                {...register("topoGrade")}
+                defaultValue={[defaultTopoGrade]}
+                onValueChange={([value]) =>
+                  setValue("topoGrade", value ?? defaultTopoGrade)
+                }
+                min={1}
+                max={ROUTE_GRADE_TO_NUMBER.size}
+                step={1}
+              />
+            </label>
+          </fieldset>
+          The nitty gritty
+          <fieldset className="flex flex-col gap-4">
+            <label htmlFor="holds" className="flex flex-col gap-2">
+              Holds
+              <input
+                {...register("holds")}
+                type="text"
+                name="holds"
+                id="holds"
+                placeholder="Hold types (crimps, jugs, underclings, pockets...)"
+                title="Holds"
+              />
+            </label>
+            <label htmlFor="profile" className="flex flex-col gap-2">
+              profile
+              <input
+                {...register("profile")}
+                type="text"
+                name="profile"
+                id="profile"
+                placeholder="Route's profile (vertical, slab, overhang...)"
+                title="profile"
+              />
+            </label>
+            <label htmlFor="height" className="flex flex-col gap-2">
+              Height
+              <input
+                {...register("height")}
+                type="number"
+                min={0}
+                max={100}
+                step={1}
+                name="height"
+                id="height"
+                placeholder="Height of the route (no need for boulders)"
+                title="height"
+              />
+            </label>
+            <label htmlFor="rating" className="flex flex-col gap-2">
+              Rating
+              <input
+                {...register("rating")}
+                min={0}
+                max={5}
+                step={1}
+                type="number"
+                name="rating"
+                id="rating"
+                placeholder="/5*"
+                title="rating"
+              />
+            </label>
+            <label htmlFor="comments" className="flex flex-col gap-2">
+              comments
+              <textarea
+                {...register("comments")}
+                name="comments"
+                id="comments"
+                placeholder="Feelings, partners, betas..."
+                title="comments"
+                autoComplete="off"
+              />
+            </label>
+          </fieldset>
+          <input type="submit" />
+        </form>
+      </div>
+    </ThemeProvider>
+  );
 }
