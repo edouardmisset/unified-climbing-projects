13ba0d898c731ffc7a9df777577002664567e510
Edouard Misset <edouardmisset@gmail.com>
2024-11-16 14:53:43 +0100

    feat: improve navigation menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,29 +2,49 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import { TRPCReactProvider } from '~/trpc/react'
 
-import '~/styles/reset.css'
-import '~/styles/colors.css'
-import '~/styles/border.css'
-import '~/styles/aspects.css'
 import '~/styles/animation.css'
+import '~/styles/aspects.css'
+import '~/styles/border.css'
 import '~/styles/brand.css'
+import '~/styles/button.css'
+import '~/styles/colors.css'
 import '~/styles/durations.css'
 import '~/styles/easing.css'
+import '~/styles/global-styles.css'
 import '~/styles/gradients.css'
+import '~/styles/normalize.css'
+import '~/styles/quick-upgrades.css'
+import '../styles/reset.css'
 import '~/styles/shadows.css'
 import '~/styles/sizes.css'
 import '~/styles/zindex.css'
-import '~/styles/quick-upgrades.css'
-import '~/styles/global-styles.css'
-import '~/styles/button.css'
-import '~/styles/normalize.css'
 
-import Link from 'next/link'
+import NextLink from 'next/link'
 
-export const metadata = {
-  title: 'Unified Climbing Projects',
-  description: 'Collection of Climbing Visualization pages',
-  icons: [{ rel: 'icon', url: '/favicon.ico' }],
+import styles from './layout.module.css'
+
+import {
+  Content,
+  Item,
+  List,
+  Link as NavLink,
+  Root,
+  Sub,
+  Trigger,
+} from '@radix-ui/react-navigation-menu'
+
+const Link = ({
+  href,
+  children,
+  ...props
+}: { children: React.ReactNode; href: string }) => {
+  return (
+    <NavLink className={styles.ListItemLink} asChild>
+      <NextLink href={href} {...props}>
+        {children}
+      </NextLink>
+    </NavLink>
+  )
 }
 
 export default function RootLayout({
@@ -38,47 +58,69 @@ export default function RootLayout({
       <body>
         <TRPCReactProvider>
           <header>
-            <nav>
-              <ul
-                className="wrap gap center space-evenly margin-auto flex-row"
-                style={{
-                  minHeight: '3rem',
-                  listStyle: 'none',
-                }}
-              >
-                <li>
-                  <Link href={'/'}>Home</Link>
-                </li>
-                <li>
+            <Root className={styles.Root}>
+              <List className={styles.List}>
+                <Item className={styles.ListItemLink}>
+                  <Link href="/">Home</Link>
+                </Item>
+                <Item className={styles.ListItemLink}>
                   <Link href={'/log'}>Log</Link>
-                </li>
-                <li>
-                  <Link href={'/graphs/ascents/pyramid'}>🧗 Pyramid</Link>
-                </li>
-                <li>
-                  <Link href={'/qr-code/ascents'}>🧗 QR</Link>
-                </li>
-                <li>
-                  <Link href={`/barcode/ascents/${thisYear}`}>🧗 Barcode</Link>
-                </li>
-                <li>
-                  <Link href={`/visualization/ascents/${thisYear}`}>
-                    🧗 Visualization
-                  </Link>
-                </li>
-                <li>
-                  <Link href={'/qr-code/training'}>💪 QR</Link>
-                </li>
-                <li>
-                  <Link href={`/barcode/training/${thisYear}`}>💪 Barcode</Link>
-                </li>
-                <li>
-                  <Link href={`/visualization/training/${thisYear}`}>
-                    💪 Visualization
-                  </Link>
-                </li>
-              </ul>
-            </nav>
+                </Item>
+
+                {/* ASCENTS */}
+                <Item className={styles.Item}>
+                  <Trigger className={styles.Trigger}>Ascents 🧗</Trigger>
+                  <Content className={styles.Content}>
+                    <Sub orientation="vertical" defaultValue="pyramid">
+                      <List className={styles.List}>
+                        <Item value="pyramid">
+                          <Link href={'/graphs/ascents/pyramid'}>
+                            🧗 Pyramid
+                          </Link>
+                        </Item>
+                        <Item value="qr-code">
+                          <Link href={'/qr-code/ascents'}>🧗 QR</Link>
+                        </Item>
+                        <Item value="barcode">
+                          <Link href={`/barcode/ascents/${thisYear}`}>
+                            🧗 Barcode
+                          </Link>
+                        </Item>
+                        <Item value="visualization">
+                          <Link href={`/visualization/ascents/${thisYear}`}>
+                            🧗 Visualization
+                          </Link>
+                        </Item>
+                      </List>
+                    </Sub>
+                  </Content>
+                </Item>
+
+                {/* TRAINING */}
+                <Item className={styles.Item}>
+                  <Trigger className={styles.Trigger}>Training 💪</Trigger>
+                  <Content className={styles.Content}>
+                    <Sub orientation="vertical" defaultValue="visualization">
+                      <List className={styles.List}>
+                        <Item className={styles.Item} value="qr-code">
+                          <Link href={'/qr-code/training'}>💪 QR</Link>
+                        </Item>
+                        <Item className={styles.Item} value="barcode">
+                          <Link href={`/barcode/training/${thisYear}`}>
+                            💪 Barcode
+                          </Link>
+                        </Item>
+                        <Item className={styles.Item} value="visualization">
+                          <Link href={`/visualization/training/${thisYear}`}>
+                            💪 Visualization
+                          </Link>
+                        </Item>
+                      </List>
+                    </Sub>
+                  </Content>
+                </Item>
+              </List>
+            </Root>
           </header>
           {children}
         </TRPCReactProvider>
@@ -86,4 +128,10 @@ export default function RootLayout({
       </body>
     </html>
   )
+}
+
+export const metadata = {
+  title: 'Unified Climbing Projects',
+  description: 'Collection of Climbing Visualization pages',
+  icons: [{ rel: 'icon', url: '/favicon.ico' }],
 }
