94bbc04ed360f626938daad859269290e5b7f02f
Edouard Misset <edouardmisset@gmail.com>
2024-11-22 17:56:10 +0100

    fix: fix grade frequency on home page

--- src/app/_components/grades-summary/grades-summary.tsx
+++ src/app/_components/grades-summary/grades-summary.tsx
@@ -13,12 +13,12 @@ export async function GradeSummary({
 }) {
   return (
     <div id="grades">
-      <h2>Grades:</h2>
+      <h2>Grades</h2>
       <p>Hardes grade: {grades.at(-1)}</p>
       <p>Average grade: {gradeAverage}</p>
       <p>
         Most climbed grade:{' '}
-        {Object.entries(gradeFrequency)
+        {gradeFrequency
           .map(([grade, count]) => [grade, count])
           .sort(([_a, a], [_b, b]) => {
             if (typeof a === 'number' && typeof b === 'number') return b - a
