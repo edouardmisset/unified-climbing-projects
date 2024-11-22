b1fb45fefc42f9a28d929c4dc95211521dff34cb
Edouard Misset <edouardmisset@gmail.com>
2024-11-22 10:17:24 +0100

    feat: add Vercel's analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-
 import { TRPCReactProvider } from '~/trpc/react'
 
 import '~/styles/animation.css'
@@ -37,6 +37,7 @@ export default function RootLayout({
           {children}
         </TRPCReactProvider>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
