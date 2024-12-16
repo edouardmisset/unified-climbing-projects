5f320ebda9dd669ddffde20044f8a475948f93ce
Edouard Misset <edouardmisset@gmail.com>
2024-12-16 11:58:06 +0100

    feat: enhance Log component with improved accessibility, formatted climbing disciplines, and refined input handling

--- src/app/log/page.tsx
+++ src/app/log/page.tsx
@@ -9,6 +9,7 @@ import {
   convertNumberToGrade,
 } from '~/helpers/converters'
 import {
+  type Ascent,
   type Grade,
   _GRADES,
   ascentStyleSchema,
@@ -19,6 +20,7 @@ import {
 
 import type { ChangeEventHandler } from 'react'
 import { GradeSlider } from '~/app/_components/slider/slider'
+import { disjunctiveListFormatter } from '~/helpers/list.ts'
 import { api } from '~/trpc/react.tsx'
 import { ClimbingStyleToggleGroup } from '../_components/toggle-group/toggle-group.tsx'
 import { onSubmit } from './actions.ts'
@@ -143,72 +145,96 @@ export default function Log(): React.JSX.Element {
     }
     // By default set the style value to 'Onsight' when the number of tries is equal to 1
     if (Number(event.target.value) === 1) {
-      setValue('style', 'Onsight')
+      if (isOnsightDisable) {
+        setValue('style', 'Flash')
+      } else {
+        setValue('style', 'Onsight')
+      }
     }
     return handleTriesChangeRegister(event)
   }
+  const unavailableDiscipline: Set<Ascent['climbingDiscipline']> = new Set([
+    'Multi-Pitch',
+  ])
+  const disciplines = climbingDiscipline.filter(
+    d => !unavailableDiscipline.has(d),
+  )
 
+  const climbingDisciplineFormattedList = disjunctiveListFormatter(disciplines)
+
+  const isOnsightDisable = watch('climbingDiscipline') === 'Boulder'
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Congrats 🎉</h1>
+      <span className="visually-hidden" aria-describedby="form-description">
+        Form to log a climbing ascent
+      </span>
       <form
+        aria-describedby="form-description"
+        autoComplete="off"
+        autoCorrect="off"
+        className={styles.form}
+        name="ascent-form"
+        spellCheck={false}
         onSubmit={handleSubmit(data => {
           onSubmit(data)
           reset()
         }, console.error)}
-        className={styles.form}
       >
         <label htmlFor="date" className={styles.label}>
           Date
           <input
-            required={true}
             {...register('date')}
+            // biome-ignore lint/a11y/noAutofocus: It would be nice to always start filling the form from here...
+            autoFocus={true}
             className={styles.input}
-            type="date"
-            title="Date"
+            enterKeyHint="next"
+            id="date"
             max={new Date().toISOString().split('T')[0]}
+            required={true}
+            title="Date"
+            type="date"
           />
         </label>
         <label htmlFor="routeName" className={styles.label}>
           Route Name
           <input
             {...register('routeName')}
-            required={true}
-            type="text"
+            autoCapitalize="on"
+            autoComplete="off"
             className={styles.input}
             id="routeName"
-            autoComplete="off"
-            placeholder="The name of the route or boulder climbed"
+            placeholder="The name of the route or boulder climbed (use `N/A` for routes without name)"
+            required={true}
             title="Route Name"
+            type="text"
           />
         </label>
         <label htmlFor="climbingDiscipline" className={styles.label}>
           Climbing Discipline
           <select
             {...register('climbingDiscipline')}
-            id="climbingDiscipline"
             className={styles.input}
-            title="Route, Boulder or Multi-Pitch"
+            id="climbingDiscipline"
+            title={climbingDisciplineFormattedList}
           >
-            {climbingDiscipline.map(discipline => {
-              const unavailableDiscipline = ['Multi-Pitch']
-              if (unavailableDiscipline.includes(discipline)) return null
-              return (
-                <option key={discipline} value={discipline}>
-                  {discipline}
-                </option>
-              )
-            })}
+            {disciplines.map(discipline => (
+              <option key={discipline} value={discipline}>
+                {discipline}
+              </option>
+            ))}
           </select>
         </label>
         <label htmlFor="crag" className={styles.label}>
           Crag
           <input
             {...register('crag')}
-            required={true}
-            id="crag"
+            autoCapitalize="on"
             className={styles.input}
+            enterKeyHint="next"
+            id="crag"
             placeholder="The name of the crag"
+            required={true}
             title="Crag Name"
             type="text"
           />
@@ -217,15 +243,16 @@ export default function Log(): React.JSX.Element {
           Area
           <input
             {...register('area')}
-            id="area"
             className={styles.input}
+            enterKeyHint="next"
+            id="area"
             placeholder="The name of the crag's sector (or area)"
             title="Crag's area"
             type="text"
           />
         </label>
         <label htmlFor="tries" className={styles.label}>
-          Tries
+          Tries & Style
           <div className={styles.tries}>
             <input
               {...triesRegister}
@@ -240,11 +267,13 @@ export default function Log(): React.JSX.Element {
               type="number"
               inputMode="numeric"
               pattern={_1To9999RegEx.source}
+              enterKeyHint="next"
             />
             <ClimbingStyleToggleGroup
               display={Number(numberOfTries) === 1}
               onValueChange={handleStyleChange}
               value={styleValue}
+              isOnsightDisable={isOnsightDisable}
             />
           </div>
         </label>
@@ -279,12 +308,13 @@ export default function Log(): React.JSX.Element {
           Holds
           <input
             {...register('holds')}
-            id="holds"
             className={styles.input}
-            placeholder={`Hold types (${holds.slice(0, 3).join(', ')}, ...)`}
-            title="Hold type"
-            type="text"
+            enterKeyHint="next"
+            id="holds"
             list="hold-types"
+            placeholder={`Hold types (${holds.slice(0, 3).join(', ')}, ...)`}
+            title="The main hold type in the route or in the crux section"
+            type="text"
           />
         </label>
         <datalist id="hold-types">
@@ -297,12 +327,13 @@ export default function Log(): React.JSX.Element {
           Profile
           <input
             {...register('profile')}
-            id="profile"
             className={styles.input}
-            placeholder={`Route's profile (${profiles.slice(0, 2).join(', ')}, ...)`}
-            title="Profile of the route"
-            type="text"
+            enterKeyHint="next"
+            id="profile"
             list="profile-types"
+            placeholder={`Route's profile (${profiles.slice(0, 2).join(', ')}, ...)`}
+            title="The main profile of the route or in the crux section"
+            type="text"
           />
         </label>
         <datalist id="profile-types">
@@ -314,43 +345,56 @@ export default function Log(): React.JSX.Element {
           Height (m)
           <input
             {...register('height')}
-            min={MIN_HEIGHT}
-            max={MAX_HEIGHT}
-            step={5}
-            id="height"
             className={styles.input}
-            placeholder="Height is not needed for boulders"
-            title="Height of the route (does not apply for boulders)"
-            type="number"
+            // TODO: Remove disabled prop - https://axesslab.com/disabled-buttons-suck/
+            disabled={watch('climbingDiscipline') === 'Boulder'}
+            enterKeyHint="next"
+            id="height"
             inputMode="numeric"
+            max={MAX_HEIGHT}
+            min={MIN_HEIGHT}
             pattern={_0To100RegEx.source}
+            placeholder="Height in meters (not needed for boulders)"
+            step={5}
+            title="Height of the route in meters (does not apply for boulders)"
+            style={
+              watch('climbingDiscipline') === 'Boulder'
+                ? {
+                    cursor: 'not-allowed',
+                  }
+                : undefined
+            }
+            type="number"
           />
         </label>
         <label htmlFor="rating" className={styles.label}>
           Rating
           <input
             {...register('rating')}
-            min={MIN_RATING}
-            max={MAX_RATING}
-            step={1}
             className={styles.input}
+            enterKeyHint="next"
             id="rating"
-            placeholder={`${MAX_RATING} ⭐️`}
-            title={`Route rating (on a ${MAX_RATING} stars system)`}
-            type="number"
             inputMode="numeric"
+            max={MAX_RATING}
+            min={MIN_RATING}
             pattern={_0To5RegEx.source}
+            placeholder={`${MAX_RATING} ⭐️`}
+            step={1}
+            title={`Route / Boulder rating (on a ${MAX_RATING} stars system)`}
+            type="number"
           />
         </label>
         <label htmlFor="comments" className={styles.label}>
           Comments
           <textarea
             {...register('comments')}
-            id="comments"
-            className={`${styles.input} ${styles.textarea}`}
-            placeholder="Feelings, partners, betas..."
-            title="Comments: Feelings, partners, betas..."
             autoComplete="off"
+            className={`${styles.input} ${styles.textarea}`}
+            enterKeyHint="send"
+            id="comments"
+            placeholder="Feelings, partners, betas..."
+            spellCheck={true}
+            title="Comments: Feelings, partners, betas..."
           />
         </label>
         <input type="submit" value="Send 📮" />
