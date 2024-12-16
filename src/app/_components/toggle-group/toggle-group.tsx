b51e405e5dd834147f06e64852b46e4276fcf6c8
Edouard Misset <edouardmisset@gmail.com>
2024-12-16 11:57:43 +0100

    feat: enhance ClimbingStyleToggleGroup with accessibility improvements and disable functionality for onsight option

--- src/app/_components/toggle-group/toggle-group.tsx
+++ src/app/_components/toggle-group/toggle-group.tsx
@@ -1,29 +1,59 @@
 import { Item, Root } from '@radix-ui/react-toggle-group'
+import { disjunctiveListFormatter } from '~/helpers/list'
+import { ascentStyle } from '~/schema/ascent'
 import styles from './toggle-group.module.css'
 
 type ClimbingStyleToggleGroup = {
   display: boolean
   onValueChange: (value: string) => void
   value?: string
+  isOnsightDisable: boolean
 }
 
 export function ClimbingStyleToggleGroup(props: ClimbingStyleToggleGroup) {
-  const { display, onValueChange, value } = props
+  const { display, onValueChange, value, isOnsightDisable } = props
 
   if (!display) return null
+
   return (
     <Root
+      aria-label={`Climbing style (${disjunctiveListFormatter(ascentStyle)}). Default is "Redpoint" if number of tries is greater than 1.`}
       className={styles.Group}
-      type="single"
       defaultValue="Redpoint"
-      value={value}
-      aria-label="climbing style"
       onValueChange={onValueChange}
+      type="single"
+      value={isOnsightDisable ? 'Flash' : value}
     >
-      <Item className={styles.Item} value="Onsight" aria-label="Left aligned">
+      <Item
+        aria-hidden={isOnsightDisable}
+        aria-label="Onsight"
+        className={styles.Item}
+        // TODO: Remove disabled prop - https://axesslab.com/disabled-buttons-suck/
+        disabled={isOnsightDisable}
+        hidden={isOnsightDisable}
+        value="Onsight"
+        style={
+          isOnsightDisable
+            ? {
+                cursor: 'not-allowed',
+                color: 'var(--gray-7)',
+              }
+            : undefined
+        }
+        title={
+          isOnsightDisable
+            ? 'Historically, boulders can only be flashed, not onsighted'
+            : 'Onsight: the route was climbed first try with no prior information (not used for boulders)'
+        }
+      >
         Onsight
       </Item>
-      <Item className={styles.Item} value="Flash" aria-label="Right aligned">
+      <Item
+        aria-label="Flash"
+        className={styles.Item}
+        title="Flash: the route was climbed first try using any prior information (also used for boulders)"
+        value="Flash"
+      >
         Flash
       </Item>
     </Root>
