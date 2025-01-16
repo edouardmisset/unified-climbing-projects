e73d1ae4f842d92a2267e43f9eb61996b1c621d0
Edouard Misset <edouardmisset@gmail.com>
2025-01-16 13:57:19 +0100

    feat: add experimental stale time configuration for dynamic and static content

--- next.config.js
+++ next.config.js
@@ -2,9 +2,16 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
+import './src/env.js'
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+      static: 180,
+    },
+  },
+}
 
-export default config;
+export default config
