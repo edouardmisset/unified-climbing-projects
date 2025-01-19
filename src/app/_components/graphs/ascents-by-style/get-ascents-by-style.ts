802f252c854c43dcf5bb76585fe3fed139272fe8
Edouard Misset <edouardmisset@gmail.com>
2025-01-19 02:16:49 +0100

    fix: handle empty filtered ascents in getAscentsByStyle function

--- src/app/_components/graphs/ascents-by-style/get-ascents-by-style.ts
+++ src/app/_components/graphs/ascents-by-style/get-ascents-by-style.ts
@@ -6,15 +6,19 @@ export const getAscentsByStyle = (ascents: Ascent[]) => {
   return ASCENT_STYLE.map(style => {
     const filteredAscentsByStyle = filterAscents(ascents, { style })
 
+    if (filteredAscentsByStyle.length === 0) {
+      return undefined
+    }
+
     return {
       id: style,
       label: style,
       value: filteredAscentsByStyle.length,
       color: fromAscentStyleToBackgroundColor(style),
     }
-  })
+  }).filter(val => val !== undefined)
 }
 
-const fromAscentStyleToBackgroundColor = (ascentStyle: Ascent['style']) => {
+function fromAscentStyleToBackgroundColor(ascentStyle: Ascent['style']) {
   return ASCENT_STYLE_TO_COLOR[ascentStyle] ?? 'var(--gray-5)'
 }
