d8904632fe910c0ad9b60f855b5520f5dcff4939
Edouard Misset <edouardmisset@gmail.com>
2025-02-17 21:35:12 +0100

    refactor: simplify Barcode component by removing unused default renderer

--- src/app/_components/barcode/barcode.tsx
+++ src/app/_components/barcode/barcode.tsx
@@ -1,34 +1,25 @@
 import type React from 'react'
 
-import { getWeek } from '~/helpers/date'
-import type { Object_ } from '~/types/generic'
+import type { ReactNode } from 'react'
+import type { Object_, StringDateTime } from '~/types/generic'
 import styles from './barcode.module.css'
-import type { BarCodeProps } from './types'
+
+export type BarCodeProps<T extends Object_> = {
+  data: ((StringDateTime & T) | undefined)[][]
+  itemRender: (
+    item: ((StringDateTime & T) | undefined)[],
+    index: number,
+  ) => ReactNode
+}
 
 export default function Barcode<T extends Object_>(
   props: BarCodeProps<T>,
 ): React.JSX.Element {
-  const { data = [] } = props
+  const { data = [], itemRender } = props
 
   return (
-    <div className={styles.barcode}>
-      {'itemRender' in props
-        ? data.map(props.itemRender)
-        : data.map((elements, index) => {
-            const firstElementDate = elements[0]?.date
-            return (
-              <span
-                key={
-                  firstElementDate ? getWeek(new Date(firstElementDate)) : index
-                }
-                style={{
-                  backgroundColor: elements[0]?.[props.field]
-                    ? 'black'
-                    : 'white',
-                }}
-              />
-            )
-          })}
+    <div className={styles.barcode} role="img">
+      {data.map((item, index) => itemRender(item, index))}
     </div>
   )
 }
