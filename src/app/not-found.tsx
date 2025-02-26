56959c3b0c9871d2bea1df99ef6ecac87b3a7868
Edouard Misset <edouardmisset@gmail.com>
2025-02-26 14:44:50 +0100

    style: update NotFound component layout for better alignment

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import { Link } from 'next-view-transitions'
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="h100 w100 flex-column justify-center align-center">
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href="/">Return Home</Link>
