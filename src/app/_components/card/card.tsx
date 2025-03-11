7b81d3deef8fb892fa002065401177d82d2190d2
Edouard Misset <edouardmisset@gmail.com>
2025-03-11 21:22:17 +0100

    style: update card component styles and class name

--- src/app/_components/card/card.tsx
+++ src/app/_components/card/card.tsx
@@ -3,5 +3,5 @@ import type React from 'react'
 import styles from './card.module.css'
 
 export async function Card({ children }: { children: React.ReactNode }) {
-  return <div className={styles.container}>{children}</div>
+  return <div className={styles.card}>{children}</div>
 }
