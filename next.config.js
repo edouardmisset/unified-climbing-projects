8c15c8d2f519d1e7634a3ec761e5809e56b75b6a
Edouard Misset <edouardmisset@gmail.com>
2025-03-23 13:11:16 +0100

    chore: add rsdoctor CLI and webpack plugin for client and server build analysis

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
  * for Docker builds.
  */
 import './src/env.js'
+// import { RsdoctorWebpackPlugin } from '@rsdoctor/webpack-plugin'
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -13,6 +14,25 @@ const config = {
       static: 180,
     },
   },
+  // webpack: config => {
+  //   if (config.name === 'client') {
+  //     config.plugins.push(
+  //       new RsdoctorWebpackPlugin({
+  //         disableClientServer: true,
+  //       }),
+  //     )
+  //   } else if (config.name === 'server') {
+  //     config.plugins.push(
+  //       new RsdoctorWebpackPlugin({
+  //         disableClientServer: true,
+  //         output: {
+  //           reportDir: './.next/server',
+  //         },
+  //       }),
+  //     )
+  //   }
+  //   return config
+  // },
 }
 
 export default config
