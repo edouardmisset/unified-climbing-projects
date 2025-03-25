8056527927d2c03e5c2f35f08ad19786482779d4
Edouard Misset <edouardmisset@gmail.com>
2025-03-25 12:23:59 +0100

    feat: refactor ascent list to use table layout for improved readability and accessibility

--- src/app/_components/ascent-list/ascent-list.tsx
+++ src/app/_components/ascent-list/ascent-list.tsx
@@ -1,6 +1,3 @@
-'use client'
-import { Separator } from '@base-ui-components/react/separator'
-import { Fragment } from 'react'
 import { NON_BREAKING_SPACE } from '~/constants/generic'
 import {
   formatCragAndArea,
@@ -14,85 +11,163 @@ import {
   fromClimbingDisciplineToEmoji,
 } from '~/helpers/formatters'
 import type { Ascent } from '~/schema/ascent'
+
 import styles from './ascent-list.module.css'
 
 export function AscentList({ ascents }: { ascents: Ascent[] }) {
   return (
-    <div className={styles.grid}>
-      {ascents.map(
-        ({
-          id,
-          routeName,
-          crag,
-          topoGrade,
-          date,
-          climbingDiscipline,
-          style,
-          tries,
-          area,
-          height,
-          holds,
-          personalGrade,
-          profile,
-          rating,
-        }) => (
-          <Fragment key={id}>
-            <span title={climbingDiscipline} className={styles.item}>
-              {fromClimbingDisciplineToEmoji(climbingDiscipline)}
-            </span>
-            <strong title={routeName} className={styles.item}>
-              {routeName}
-            </strong>
-            <em
-              title={`${topoGrade} - ${personalGrade}`}
-              className={`${styles.item} monospace`}
-            >
-              {topoGrade.endsWith('+')
-                ? topoGrade
-                : `${topoGrade}${NON_BREAKING_SPACE}`}
-              {personalGrade === topoGrade ? undefined : (
-                <sup> {personalGrade}</sup>
-              )}
-            </em>
-            <span
-              title={tries === 1 ? style : formatOrdinals(tries)}
-              className={styles.item}
-            >
-              <span>{fromAscentStyleToEmoji(style)}</span>
-              <sup>{tries > 1 ? ` ${formatOrdinals(tries)}` : ''}</sup>
-            </span>
-            <span
-              title={formatDateInTooltip(date, 'longDate')}
-              className={`${styles.item} monospace`}
-            >
-              {formatDateInTooltip(date, 'shortDate')}
-            </span>
-
-            <span title={formatCragAndArea(crag, area)} className={styles.item}>
-              {formatCragAndArea(crag, area)}
-            </span>
-
-            <span title={`${height}m`} className={`${styles.item} monospace`}>
-              {formatHeight(height)}
-            </span>
-            <span title={profile} className={styles.item}>
-              {formatProfile(profile)}
-            </span>
-            <span title={holds} className={styles.item}>
-              {formatHolds(holds)}
-            </span>
-
-            <span title={`${rating}⭐️`} className={styles.item}>
-              {formatRating(rating)}
-            </span>
-          </Fragment>
-        ),
-      )}
-      <Separator orientation="horizontal" className={styles.Separator} />
-      <div className={styles.total}>
-        Total: <strong>{ascents.length}</strong> ascent
-        {ascents.length > 1 ? 's' : ''}
-      </div>
-    </div>
+    <table className={styles.table}>
+      <thead className={styles.header}>
+        <tr className={styles.headerRow}>
+          <th
+            className={`${styles.cell} ${styles.headerCell}`}
+            title="Route, boulder..."
+          >
+            Discipline
+          </th>
+          <th
+            className={`${styles.cell} ${styles.headerCell}`}
+            title="Name of the route"
+          >
+            Name
+          </th>
+          <th
+            className={`${styles.cell} ${styles.headerCell}`}
+            title="French grade of the route as shown in the guide book"
+          >
+            Grade
+          </th>
+          <th
+            className={`${styles.cell} ${styles.headerCell}`}
+            title="Style of the ascent (onsight, flash, redpoint) and # tries"
+          >
+            Style
+          </th>
+          <th
+            className={`${styles.cell} ${styles.headerCell}`}
+            title="Date of the ascent"
+          >
+            Date
+          </th>
+          <th
+            className={`${styles.cell} ${styles.headerCell}`}
+            title="Location (crag and sector if available) of the ascent"
+          >
+            Location
+          </th>
+          <th
+            className={`${styles.cell} ${styles.headerCell}`}
+            title="Height of the route in meters"
+          >
+            Height (m)
+          </th>
+          <th
+            className={`${styles.cell} ${styles.headerCell}`}
+            title="General profile of the route or profile of the crux section"
+          >
+            Profile
+          </th>
+          <th
+            className={`${styles.cell} ${styles.headerCell}`}
+            title="Most common holds on the route (or boulder) or holds of the crux section"
+          >
+            Holds
+          </th>
+          <th
+            className={`${styles.cell} ${styles.headerCell}`}
+            title="Rating of the ascent (out of 5 stars)"
+          >
+            Rating
+          </th>
+        </tr>
+      </thead>
+      <tbody className={styles.body}>
+        {ascents.map(
+          ({
+            id,
+            routeName,
+            crag,
+            topoGrade,
+            date,
+            climbingDiscipline,
+            style,
+            tries,
+            area,
+            height,
+            holds,
+            personalGrade,
+            profile,
+            rating,
+          }) => (
+            <tr key={id} className={styles.row}>
+              <td title={climbingDiscipline} className={styles.cell}>
+                {fromClimbingDisciplineToEmoji(climbingDiscipline)}
+              </td>
+              <td className={styles.cell}>
+                <strong title={routeName}>{routeName}</strong>
+              </td>
+              <td className={styles.cell}>
+                <em
+                  title={`${topoGrade} - ${personalGrade}`}
+                  className="monospace"
+                >
+                  <span>
+                    {topoGrade.endsWith('+')
+                      ? topoGrade
+                      : `${topoGrade}${NON_BREAKING_SPACE}`}
+                  </span>
+                  {personalGrade === topoGrade ? undefined : (
+                    <sup> {personalGrade}</sup>
+                  )}
+                </em>
+              </td>
+              <td
+                title={tries === 1 ? style : formatOrdinals(tries)}
+                className={styles.cell}
+              >
+                <span>{fromAscentStyleToEmoji(style)}</span>
+                <sup>{tries > 1 ? ` ${formatOrdinals(tries)}` : ''}</sup>
+              </td>
+              <td
+                title={formatDateInTooltip(date, 'longDate')}
+                className={`${styles.cell} monospace`}
+              >
+                {formatDateInTooltip(date, 'shortDate')}
+              </td>
+              <td title={formatCragAndArea(crag, area)} className={styles.cell}>
+                {formatCragAndArea(crag, area)}
+              </td>
+              <td
+                title={height === undefined ? undefined : `${height}m`}
+                className={`${styles.cell} monospace`}
+              >
+                {formatHeight(height)}
+              </td>
+              <td title={profile} className={styles.cell}>
+                {formatProfile(profile)}
+              </td>
+              <td title={holds} className={styles.cell}>
+                {formatHolds(holds)}
+              </td>
+              <td
+                title={rating === undefined ? undefined : `${rating}⭐️`}
+                className={styles.cell}
+              >
+                {formatRating(rating)}
+              </td>
+            </tr>
+          ),
+        )}
+      </tbody>
+      <tfoot className={styles.footer}>
+        <tr className={styles.row}>
+          <th className={styles.footerCell}>Total</th>
+          <td className={styles.footerCell}>
+            <strong>{ascents.length}</strong> ascent
+            {ascents.length > 1 ? 's' : ''}
+          </td>
+        </tr>
+      </tfoot>
+    </table>
   )
 }
