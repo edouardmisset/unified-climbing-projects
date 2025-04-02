a3ff523d1d48393acf08d55b1f655ce4ac6432a5
Edouard Misset <edouardmisset@gmail.com>
2025-04-02 22:51:56 +0200

    docs: add jsdoc to get-ascents-by-grades-per-crag

--- src/app/_components/charts/ascents-by-grades-per-crag/get-ascents-by-grades-per-crag.ts
+++ src/app/_components/charts/ascents-by-grades-per-crag/get-ascents-by-grades-per-crag.ts
@@ -3,6 +3,21 @@ import { createGradeScale } from '~/helpers/create-grade-scale'
 import { minMaxGrades } from '~/helpers/min-max-grades'
 import type { Ascent } from '~/schema/ascent'
 
+/**
+ * Aggregates ascents by crag and computes the frequency of each climbing grade.
+ *
+ * The function groups the provided ascents by their `crag` property and determines
+ * the grade scale based on the minimum and maximum grades encountered in each
+ * group.
+ * It then creates a frequency object per crag that maps each grade within the scale
+ * to its corresponding count and assigns a background color for the grade.
+ *
+ * @param {Ascent[]} ascents - An array of ascent objects containing climb details.
+ * @returns {Record<string, string | number>[]} An array of objects where each object includes:
+ *  - `crag`: The crag name.
+ *  - A count for each grade present in the grade scale.
+ *  - A `<grade>Color` property for each grade representing its assigned background color.
+ */
 export function getAscentsByGradesPerCrag(
   ascents: Ascent[],
 ): Record<string, string | number>[] {
@@ -33,11 +48,13 @@ export function getAscentsByGradesPerCrag(
 
     for (const { topoGrade } of ascents) {
       if (
-        frequency[topoGrade] !== undefined &&
-        typeof frequency[topoGrade] === 'number'
+        frequency[topoGrade] === undefined ||
+        typeof frequency[topoGrade] !== 'number'
       ) {
-        frequency[topoGrade] += 1
+        continue
       }
+
+      frequency[topoGrade] += 1
     }
 
     return frequency
