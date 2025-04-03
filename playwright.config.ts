3ea52be152d913c47d56f9b8a923987927adf803
Edouard Misset <edouardmisset@gmail.com>
2025-04-03 11:59:16 +0200

    fix: update web server command in Playwright config to use Bun

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 /**
  * Read environment variables from file.
@@ -72,8 +72,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'bun run preview',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
   },
-});
+})
