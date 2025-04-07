c95a140607c1bdcbfc185c5439ad687f952a41fc
Edouard Misset <edouardmisset@gmail.com>
2025-04-07 17:59:54 +0200

    feat: add NotFound component for empty top ten ascents

--- src/app/_components/top-ten-table/top-ten-table.tsx
+++ src/app/_components/top-ten-table/top-ten-table.tsx
@@ -1,9 +1,12 @@
+import NotFound from '~/app/not-found'
 import type { Ascent } from '~/schema/ascent'
 import { AscentList } from '../ascent-list/ascent-list'
 
 export function TopTenTable({
   topTenAscents,
 }: { topTenAscents: Ascent[] }): React.JSX.Element {
+  if (topTenAscents.length === 0) return <NotFound />
+
   return (
     <AscentList ascents={topTenAscents} showDetails={false} showPoints={true} />
   )
