3de72afcf13653ec1ca416342e79609ba7541cee
Edouard Misset <edouardmisset@gmail.com>
2025-06-19 20:38:25 +0200

    refactor: simplify Card component structure by removing unnecessary wrapper

--- src/app/_components/card/card.tsx
+++ src/app/_components/card/card.tsx
@@ -3,9 +3,5 @@ import type React from 'react'
 import styles from './card.module.css'
 
 export async function Card({ children }: { children: React.ReactNode }) {
-  return (
-    <div className={styles.border}>
-      <div className={styles.card}>{children}</div>
-    </div>
-  )
+  return <div className={styles.card}>{children}</div>
 }
