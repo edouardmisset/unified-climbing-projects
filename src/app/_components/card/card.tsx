dea7397ce81f34a0168904911983008adfc66ab0
Edouard Misset <edouardmisset@gmail.com>
2025-07-06 00:33:10 +0200

    feat: add card border to the graphs in the dashboard

--- src/app/_components/card/card.tsx
+++ src/app/_components/card/card.tsx
@@ -1,6 +1,6 @@
 import type React from 'react'
 import styles from './card.module.css'
 
-export async function Card({ children }: { children: React.ReactNode }) {
+export function Card({ children }: { children: React.ReactNode }) {
   return <div className={styles.card}>{children}</div>
 }
