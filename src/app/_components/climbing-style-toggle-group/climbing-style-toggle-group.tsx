eefed38050311c4ea2e332b960500d21000685dc
Edouard Misset <edouardmisset@gmail.com>
2025-07-31 14:34:47 +0200

    refactor(ClimbingStyleToggleGroup): reorganize props type definition

--- src/app/_components/climbing-style-toggle-group/climbing-style-toggle-group.tsx
+++ src/app/_components/climbing-style-toggle-group/climbing-style-toggle-group.tsx
@@ -1,16 +1,10 @@
 import { Toggle } from '@base-ui-components/react/toggle'
 import { ToggleGroup } from '@base-ui-components/react/toggle-group'
+import type { CSSProperties } from 'react'
 import { disjunctiveListFormatter } from '~/helpers/list-formatter'
 import { ASCENT_STYLE } from '~/schema/ascent'
 import { ClimbingStyle } from '../climbing/climbing-style/climbing-style'
 import styles from './climbing-style-toggle-group.module.css'
-
-type ClimbingStyleToggleGroupProps = {
-  display: boolean
-  onValueChange: (groupValue: string[], event: Event) => void
-  value?: string
-  isOnsightDisable: boolean
-}
 
 export function ClimbingStyleToggleGroup(
   props: Omit<ToggleGroup.Props, 'value'> & ClimbingStyleToggleGroupProps,
@@ -32,17 +26,9 @@ export function ClimbingStyleToggleGroup(
         aria-hidden={isOnsightDisable}
         aria-label="Onsight"
         className={`contrast-color ${styles.Button}`}
-        // TODO: Remove disabled prop - https://axesslab.com/disabled-buttons-suck/
         disabled={isOnsightDisable}
         hidden={isOnsightDisable}
-        style={
-          isOnsightDisable
-            ? {
-                color: 'var(--gray-7)',
-                cursor: 'not-allowed',
-              }
-            : undefined
-        }
+        style={isOnsightDisable ? disabledStyle : undefined}
         title={
           isOnsightDisable
             ? 'Historically, boulders can only be flashed, not onsighted'
@@ -62,4 +48,16 @@ export function ClimbingStyleToggleGroup(
       </Toggle>
     </ToggleGroup>
   )
+}
+
+const disabledStyle: CSSProperties = {
+  color: 'var(--gray-7)',
+  cursor: 'not-allowed',
+}
+
+type ClimbingStyleToggleGroupProps = {
+  display: boolean
+  onValueChange: (groupValue: string[], event: Event) => void
+  value?: string
+  isOnsightDisable: boolean
 }
