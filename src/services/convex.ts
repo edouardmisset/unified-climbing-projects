0ee928c7088cb80ea1d4dc32984f6a8cdde59da9
Edouard Misset <edouardmisset@gmail.com>
2025-09-22 14:44:31 +0200

    build: fix build

--- src/services/convex.ts
+++ src/services/convex.ts
@@ -1,14 +1,25 @@
-import { api } from 'convex/_generated/api'
 import { fetchAction, fetchQuery } from 'convex/nextjs'
+import { api } from '~/../convex/_generated/api'
 import { EMPTY_OBJECT } from '~/constants/generic'
-import type { Ascent } from '~/schema/ascent'
-import type { TrainingSession } from '~/schema/training'
+import { type Ascent, ascentSchema } from '~/schema/ascent'
+import { type TrainingSession, trainingSessionSchema } from '~/schema/training'
 
 /** ASCENTS */
 
 export async function getAllAscentsFromDB(): Promise<Ascent[]> {
   try {
-    return await fetchQuery(api.ascents.get, EMPTY_OBJECT)
+    const ascentData = await fetchQuery(api.ascents.get, EMPTY_OBJECT)
+
+    const parsedAscents = ascentSchema.array().safeParse(ascentData)
+    if (!parsedAscents.success) {
+      globalThis.console.error(
+        'Error parsing ascents from DB:',
+        parsedAscents.error,
+      )
+      return []
+    }
+
+    return parsedAscents.data
   } catch (error) {
     globalThis.console.error('Error fetching ascents from DB:', error)
     return []
@@ -29,7 +40,16 @@ export async function getAllTrainingSessionsFromDB(): Promise<
   TrainingSession[]
 > {
   try {
-    return await fetchQuery(api.training.get, EMPTY_OBJECT)
+    const trainingData = await fetchQuery(api.training.get, EMPTY_OBJECT)
+    const parsedTraining = trainingSessionSchema.array().safeParse(trainingData)
+    if (!parsedTraining.success) {
+      globalThis.console.error(
+        'Error parsing training sessions from DB:',
+        parsedTraining.error,
+      )
+      return []
+    }
+    return parsedTraining.data
   } catch (error) {
     globalThis.console.error('Error fetching training sessions from DB:', error)
     return []
