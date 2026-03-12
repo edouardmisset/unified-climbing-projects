aa775d4bbed2c4de6e474e5d50335c6280d8c4fc
Edouard Misset <edouardmisset@gmail.com>
2026-03-12 09:35:52 +0100

    style: update ThemeToggle component to use a switch input with star and cloud animations

--- src/app/_components/theme-toggle/theme-toggle.tsx
+++ src/app/_components/theme-toggle/theme-toggle.tsx
@@ -1,4 +1,3 @@
-import { Moon, Sun } from 'lucide-react'
 import { useCallback } from 'react'
 import styles from './theme-toggle.module.css'
 
@@ -12,14 +11,25 @@ export function ThemeToggle({
   const handleThemeChange = useCallback(() => onChange(!checked), [checked, onChange])
 
   return (
-    <button
-      aria-label='Toggle theme'
-      className={`${styles.toggle} ${checked ? styles.isDark : styles.isLight}`}
-      onClick={handleThemeChange}
-      type='button'
-    >
-      <Sun className={`${styles.icon} ${styles.sun}`} />
-      <Moon className={`${styles.icon} ${styles.moon}`} />
-    </button>
+    <label className={styles.switch}>
+      <input
+        aria-label='Toggle theme'
+        checked={!checked}
+        onChange={handleThemeChange}
+        type='checkbox'
+      />
+      <span className={styles.slider}>
+        <div className={`${styles.star} ${styles.star_1}`} />
+        <div className={`${styles.star} ${styles.star_2}`} />
+        <div className={`${styles.star} ${styles.star_3}`} />
+        <svg className={`${styles.cloud_1} ${styles.cloud}`} viewBox='0 0 16 16'>
+          <path
+            d='m391.84 540.91c-.421-.329-.949-.524-1.523-.524-1.351 0-2.451 1.084-2.485 2.435-1.395.526-2.388 1.88-2.388 3.466 0 1.874 1.385 3.423 3.182 3.667v.034h12.73v-.006c1.775-.104 3.182-1.584 3.182-3.395 0-1.747-1.309-3.186-2.994-3.379.007-.106.011-.214.011-.322 0-2.707-2.271-4.901-5.072-4.901-2.073 0-3.856 1.202-4.643 2.925'
+            fill='#fff'
+            transform='matrix(.77976 0 0 .78395-299.99-418.63)'
+          />
+        </svg>
+      </span>
+    </label>
   )
 }
